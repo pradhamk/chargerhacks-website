ee51e16fc445e4a71ed53f3e4b89823c067c5dd1
Pradham K <email@pradhamk.com>
2023-07-21 11:41:59 -0700

    404 page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script>
+    import '~/assets/css/error.css'
+</script>
+
+
+<template>
+    <div class='not_found_div'>
+        <h1 class='not_found_text'>Sorry! That page was not found</h1>
+        <a href="/">
+            <button class='goto_button'>
+                Go to Home
+            </button>
+        </a>
+    </div>
+    <LinkBackground />
+</template>
